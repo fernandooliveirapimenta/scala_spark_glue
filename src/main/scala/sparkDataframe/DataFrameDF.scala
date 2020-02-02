60f25d535781e6da173b75ba93944691c4d8a2fd
Fernando Pimenta <fernando.pimenta107@gmail.com>
2020-02-01 22:18:45 -0300

    spark

--- src/main/scala/sparkDataframe/DataFrameDF.scala
+++ src/main/scala/sparkDataframe/DataFrameDF.scala
@@ -1,15 +1,32 @@
-package sparkDataframe
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.DataFrame
 
+val spark: SparkSession = SparkSession.builder().master("local").getOrCreate()
 
-object DataFrameDF extends App{
+val df: DataFrame = spark.read.option("header", "true").option("inferSchema", "true").csv("CitiGroup2006_2008")
 
-  import org.apache.spark.sql.SparkSession
+//for(row <- df.head(5)){
+//  println(row)
+//}
 
-  val spark = SparkSession
-    .builder()
-    .master("local")
-    .getOrCreate()
+df.columns
+println()
+println()
+df.describe().show()
+println()
+println()
+println()
+df.select("Volume").show()
+println()
+println()
+df.select("Date","Close").show()
+println()
+println()
 
-//  val df = spark.read.csv("")
+val df2: DataFrame= df.withColumn("HighPlusLow", df("High") + df("Low"))
+df2.printSchema()
 
-}
+println()
+println()
+
+df2.select(df2("HighPlusLow").as("HPL"), df2("Close")).show()
---
+++ src/main/scala/sparkDataframe/DataFrameOperations.scala
@@ -0,0 +1,44 @@
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+val spark: SparkSession = SparkSession.builder().getOrCreate()
+
+val df: DataFrame = spark.read.option("header", "true").option("inferSchema", "true").csv("CitiGroup2006_2008")
+
+df.printSchema()
+
+////////////////
+////////////////
+
+import  spark.implicits._
+
+df.filter($"Close">492).orderBy($"Close").show()
+
+
+println()
+println()
+
+df.filter("Open = 496.9").show()
+
+println()
+println()
+
+df.filter($"Close" < 480 && $"High" < 480).show()
+
+println()
+df.filter("Close < 480 AND High < 480").show()
+
+println()
+println()
+
+val CH_low: Array[Row] = df.filter($"Close" < 480 && $"High" < 480).collect()
+
+val ChLowCount: Long = df.filter("Close < 480 AND High < 480").count()
+
+println()
+
+df.filter("High = 484.40").show()
+
+println()
+
+df.select(corr("High","Low")).show()
+
